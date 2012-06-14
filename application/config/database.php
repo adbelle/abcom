324eef37d84ef578247bb6dee5ae3d0fecd43962
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-14 08:00:35 -0400

    Change in database setup

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'zeonfederated.com';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'olioliolio';
 $db['default']['database'] = 'anthonybellew';
