bcb3f686f79f51c88b1d0061fd87b2e730767eab
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-20 06:50:27 -0400

    shoutouts to my homiez at ZF\!

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,16 +1,20 @@
 <div class="grid-two-thirds">
 Hello World!<br /><br />
 
-This is my homepage. Er, rather, where my homepage will be.<br /><br />
+I am in the process of building a new, more impressive homepage.<br /><br />
 
 You can follow development progress <a href="/news/">here.</a><br /><br />
 </div><!--grid-two-thirds-->
 
 <div class="grid-one-third">
-This is purple.<br /><br />
+This page will serve as my personal portfolio.<br /><br />
 
-I am colorblind.<br /><br />
+My business partners at Zeon Federated are
+<a href="http://zeonfederated.com/">Eugen Rochko</a> and
+<a href="http://acostoss.com/">Jesse Hamilton</a>.
+<br /><br />
 
-My other website is <a href="http://thecolorless.net/">here.</a><br /><br />
+My other websites are <a href="http://thecolorless.net/">here.</a>
+and <a href="http://artistsnclients.com/">here.</a><br /><br />
 </div><!--grid-one-thirds-->
 
