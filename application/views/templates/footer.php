29fd181a2f14e16ae2c6ec5c68c43c1debf7bf39
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 10:15:09 -0400

    link in footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <br />
 <br />
-	<div class="grid-12"><strong>&copy; 2012 Anthony Bellew</strong><br /></div>	
+	<div class="grid-12"><strong>&copy; 2012 <a href="mailto:anthonybellew.com">Anthony Bellew</a></strong><br /></div>	
 
   </div><!--grids-->
 </div><!--wrap-->
