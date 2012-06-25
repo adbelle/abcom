2551d86980e12539c11f52e441fb7b7022028082
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 11:26:24 -0400

    updated main page links

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,11 @@
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/about0.jpg" height="200"></img></a> </div><div align="center">About Me</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/resume60.jpg" height="200"></img></a> </div><div align="center">Resume and CV</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/code120.jpg" height="200"></img></a> </div><div align="center">Code</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/background"><img src="http://anthonybellew.com/public/images/about0.jpg" height="200"></img></a> </div><div align="center">About Me</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/resume"><img src="http://anthonybellew.com/public/images/resume60.jpg" height="200"></img></a> </div><div align="center">Resume and CV</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="https://github.com/TrevInc/following"><img src="http://anthonybellew.com/public/images/code120.jpg" height="200"></img></a> </div><div align="center">Code</div></div>
 
 </br></br><hr /></br>
 
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/music180.jpg" height="200"></img></a> </div><div align="center">Music</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/music"><img src="http://anthonybellew.com/public/images/music180.jpg" height="200"></img></a> </div><div align="center">Music</div></div>
 <div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/blog240.jpg" height="200"></img></a> </div><div align="center">Blog</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/design300.jpg" height="200"></img></a> </div><div align="center">Design</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/design"><img src="http://anthonybellew.com/public/images/design300.jpg" height="200"></img></a> </div><div align="center">Design</div></div>
 
 
--- application/views/pages/home.php~
+++ application/views/pages/home.php~
@@ -1,5 +1,5 @@
 <div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/about0.jpg" height="200"></img></a> </div><div align="center">About Me</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Resume and CV</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/resume60.jpg" height="200"></img></a> </div><div align="center">Resume and CV</div></div>
 <div class="grid-4 picframe"> <div align="center"><a href="http://anthonybellew.com/news/"><img src="http://anthonybellew.com/public/images/code120.jpg" height="200"></img></a> </div><div align="center">Code</div></div>
 
 </br></br><hr /></br>
