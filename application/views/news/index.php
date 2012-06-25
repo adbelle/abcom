6d0abbd9d6605bcb45fc45b28b7bf2244674b96c
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 14:51:00 -0400

    visibility flag

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,6 @@
 <div class="grid-12">
-<?php foreach ($news as $news_item): ?>
+<?php foreach ($news as $news_item): 
+	if $news_item['visible'] = '1' {?>
 
     <h4><?php echo $news_item['title'] ?></h4>
     <div id="main">
@@ -8,5 +9,5 @@
     <p><a href="/news/<?php echo $news_item['slug'] ?>">View article</a></p>
 	<br />
 
-<?php endforeach ?>
+<?php } endforeach ?>
 </div>
