305b86de5bdf48df665e38f15e14daeae523a1b0
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 14:52:34 -0400

    visibility flag, take 3

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,6 +1,6 @@
 <div class="grid-12">
 <?php foreach ($news as $news_item): 
-	if ($news_item['visible'] = '1') { ?>
+	if ($news_item['visible'] == '1') { ?>
 
     <h4><?php echo $news_item['title'] ?></h4>
     <div id="main">
