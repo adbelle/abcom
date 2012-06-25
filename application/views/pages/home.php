aa0c012f546e0ca680c20618ac0df76ebf79ef7d
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 10:28:18 -0400

    test align tag

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,5 @@
 <div class="grid-4 picframe"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div>
-<div class="grid-4 picframe"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div>
+<div class="grid-4 picframe"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" align="center"></img></a> </div>
 <div class="grid-4 picframe"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div>
 </br></br><hr /></br>
 <div class="grid-4 picframe"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div>
