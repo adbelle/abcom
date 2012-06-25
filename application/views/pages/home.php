fc1516ec60854767583a17b1dfd3ef0becd27414
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 10:37:44 -0400

    test of 3x2 layout take 5

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,11 @@
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">About Me</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/linuxmint.jpg" height="200"></img></a> </div><div align="center">Resume and CV</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/hedge.jpg" height="200"></img></a> </div><div align="center">Code</div></div>
 
 </br></br><hr /></br>
 
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Music</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg" height="200"></img></a> </div><div align="center">Blog</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/anc.jpg" height="200"></img></a> </div><div align="center">Design</div></div>
 
 
