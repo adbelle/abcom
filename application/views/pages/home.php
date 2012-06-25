78db9d244d0edb234a558a4367873bb028ca1bfa
Anthony Bellew <adbelle@SkynetMint13.(none)>
2012-06-25 10:31:40 -0400

    test of 3x2 layout take 3.1

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,10 +1,10 @@
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
 
 </br></br><hr /></br>
 
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
-<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content<div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
+<div class="grid-4 picframe"> <div align="center"><a href="http://thecolorless.net/"><img src="http://anthonybellew.com/public/images/cl_thumb.jpg"></img></a> </div><div align="center">Content</div></div>
 
